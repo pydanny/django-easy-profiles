9cbfe01c1450896f60fea68e47e64d540dbe2a15
pydanny <pydanny@gmail.com>
2011-07-15 16:57:44 -0700

    Moar tweaks

--- test_project/profiles/models.py
+++ test_project/profiles/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from easy_profiles.models import ProfileBase
 
-# Create your models here.
+
+class Profile(ProfileBase):
+    pass
+    
+class MultipleProfileBase(models.Model):    
+    """ Use when your project has more than one app"""
+    pass
