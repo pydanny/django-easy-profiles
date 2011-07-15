3be77197d72b11e87aa09ba192ec5e24435933da
pydanny <pydanny@gmail.com>
2011-07-15 16:58:28 -0700

    holding off on that

--- test_project/profiles/models.py
+++ test_project/profiles/models.py
@@ -3,7 +3,3 @@ from easy_profiles.models import ProfileBase
 
 class Profile(ProfileBase):
     pass
-    
-class MultipleProfileBase(models.Model):    
-    """ Use when your project has more than one app"""
-    pass
